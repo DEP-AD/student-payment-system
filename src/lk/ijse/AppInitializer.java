f8c7452d2dec16b825bf87da8643d158a31f8e06
Aruni Dissanayake <aruni.saku@gmail.com>
2021-06-15 06:02:06 +0530

    Initial commit

---
+++ src/lk/ijse/AppInitializer.java
@@ -0,0 +1,21 @@
+package lk.ijse;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class AppInitializer extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(this.getClass().getResource("/lk/ijse/view/LoginForm.fxml"));
+
+    }
+}
